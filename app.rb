2c3a2c86eba2631f1d1ba77f396ed09802aa7fb7
Joy Lin <vvlnote@hotmail.com>
2019-04-30 17:13:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ module FormsLab
       @pirate = Pirate.new(name: params[:pirate][:name],
       height: params[:pirate][:height], weight: params[:pirate][:weight])
       params[:pirate][:ships].each do |ship|
-        ship = Ship.new(name: ship[:name], type: ship[:category], booty: ship[:booty])
+        ship = Ship.new(name: ship[:name], type: ship[:type], booty: ship[:booty])
         ship.pirate = @pirate
         @pirate.ships << ship
       end
